e0f6bb942a52a3dd4c3d5f6c1404607e48e60a25
Sven Nilsen <bvssvni@gmail.com>
2014-06-10 09:29:17 +0200

    Updated docs

--- docs/rust-image/implementors/image/pixels/trait.Pixel.js
+++ docs/rust-image/implementors/image/pixels/trait.Pixel.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['image'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='image/vp8/struct.Frame.html' title='image::vp8::Frame'>Frame</a>",];
+implementors['image'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
