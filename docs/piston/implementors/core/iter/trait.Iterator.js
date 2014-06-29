fe4a1c5fc7e87aecb13dcb0b79fdb6e81aee72e9
Leon <leon@Leons-MacBook-Pro.local>
2014-06-29 22:56:18 +0200

    Updated Docs. Removed docs for workspace forks

---
+++ docs/piston/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['piston'] = ["impl&lt;'a, W: <a class='trait' href='piston/game_window/trait.GameWindow.html' title='piston::game_window::GameWindow'>GameWindow</a>&gt; <a class='trait' href='http://doc.rust-lang.org/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;<a class='type' href='piston/type.GameEvent.html' title='piston::GameEvent'>GameEvent</a>&gt; for <a class='struct' href='piston/struct.GameIterator.html' title='piston::GameIterator'>GameIterator</a>&lt;'a, W&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/rust-image/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['image'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;<a class='struct' href='image/struct.SubImage.html' title='image::SubImage'>SubImage</a>&lt;'a&gt;&gt; for <a class='struct' href='image/struct.Tiles.html' title='image::Tiles'>Tiles</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
