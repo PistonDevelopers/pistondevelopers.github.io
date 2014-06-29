105772a6b65d5d027a763037440245bfb56b3e7e
Leon <leon@Leons-MacBook-Pro.local>
2014-06-29 23:07:38 +0200

    Updated Docs

---
+++ docs/rust-image/implementors/core/collections/trait.Collection.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['image'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/collections/trait.Collection.html' title='core::collections::Collection'>Collection</a> for <a class='type' href='image/imaging/pixelbuf/type.PixelBuf.html' title='image::imaging::pixelbuf::PixelBuf'>PixelBuf</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
