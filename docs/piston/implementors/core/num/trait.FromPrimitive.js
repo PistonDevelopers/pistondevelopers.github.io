d1e3d5512d8cdec4b92fdf8877362d92b29da7c8
Kevin Walter <kevin.walter.private@googlemail.com>
2014-08-25 16:44:22 +0200

    Update piston docs

--- docs/piston/implementors/core/num/trait.FromPrimitive.js
+++ docs/piston/implementors/core/num/trait.FromPrimitive.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
-implementors['piston'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='type' href='piston/keyboard/type.Key.html' title='piston::keyboard::Key'>Key</a>",];
+implementors['input'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='type' href='input/keyboard/type.Key.html' title='input::keyboard::Key'>Key</a>",];
+implementors['piston'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='type' href='image/png/filter/type.FilterType.html' title='image::png::filter::FilterType'>FilterType</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='type' href='piston/input/keyboard/type.Key.html' title='piston::input::keyboard::Key'>Key</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ docs/piston/implementors/core/ops/trait.BitOr.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors['device'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a>&lt;<a class='struct' href='device/state/struct.ColorMask.html' title='device::state::ColorMask'>ColorMask</a>, <a class='struct' href='device/state/struct.ColorMask.html' title='device::state::ColorMask'>ColorMask</a>&gt; for <a class='struct' href='device/state/struct.ColorMask.html' title='device::state::ColorMask'>ColorMask</a>",];
+implementors['gfx'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a>&lt;<a class='struct' href='gfx/state/struct.ColorMask.html' title='gfx::state::ColorMask'>ColorMask</a>, <a class='struct' href='gfx/state/struct.ColorMask.html' title='gfx::state::ColorMask'>ColorMask</a>&gt; for <a class='struct' href='gfx/state/struct.ColorMask.html' title='gfx::state::ColorMask'>ColorMask</a>",];
+implementors['piston'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a>&lt;<a class='struct' href='cam/first_person/struct.Keys.html' title='cam::first_person::Keys'>Keys</a>, <a class='struct' href='cam/first_person/struct.Keys.html' title='cam::first_person::Keys'>Keys</a>&gt; for <a class='struct' href='cam/first_person/struct.Keys.html' title='cam::first_person::Keys'>Keys</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a>&lt;<a class='struct' href='piston/gfx/state/struct.ColorMask.html' title='piston::gfx::state::ColorMask'>ColorMask</a>, <a class='struct' href='piston/gfx/state/struct.ColorMask.html' title='piston::gfx::state::ColorMask'>ColorMask</a>&gt; for <a class='struct' href='piston/gfx/state/struct.ColorMask.html' title='piston::gfx::state::ColorMask'>ColorMask</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
